a7cc31372a255e951eb1349f99aadd39d6c48235
Thanakrit Rojsakthavorn <thanakrit.ro@arise.tech>
2024-07-10 20:42:20 +0700

    [level1:R3:5] test 5

--- level1/l1.go
+++ level1/l1.go
@@ -5,11 +5,19 @@ import "strconv"
 
 func FizzbuzzLevel1(number int) string {
 	text := ""
-
+	flag := false
 
 	if number == 3 {
 		text = "Fizz"
-	} else {
+		flag = true
+	} 
+	
+	if number == 5 {
+		text = "Buzz"
+		flag = true
+	}
+	
+	if !flag {
 		text = strconv.Itoa(number)
 	}
 	return text
