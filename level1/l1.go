268d87ccabfd5142fc33ef04fc52eecd26c88bdd
Thanakrit Rojsakthavorn <thanakrit.ro@arise.tech>
2024-07-10 20:26:08 +0700

    [level1:R2:1] test 1

--- level1/l1.go
+++ level1/l1.go
@@ -1,16 +1,6 @@
 package level1
 
-import "strconv"
 
 func FizzbuzzLevel1(number int) string {
-	if number == 15 {
-		return "FizzBuzz"
-	}
-	if number == 5 || number == 10{
-		return "Buzz"
-	}
-	if number % 3 == 0{
-		return "Fizz"
-	}
-	return strconv.Itoa(number)
+	return "1"
 }
