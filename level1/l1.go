448e4d8d75fe3cb8ef73d3fae9e04e1d45d0f38c
Thanakrit Rojsakthavorn <thanakrit.ro@arise.tech>
2024-07-10 12:44:46 +0700

    [Level1:2] testcase 2

--- level1/l1.go
+++ level1/l1.go
@@ -1,5 +1,8 @@
 package level1
 
 func FizzbuzzLevel1(number int) string {
+	if number == 2 {
+		return "2"
+	}
 	return "1"
 }
--- level1/l1_test.go
+++ level1/l1_test.go
@@ -10,3 +10,11 @@ func TestFizzBuzzLevel1(t *testing.T){
 		t.Errorf("got %q but want %q", got, want)
 	}
 }
+func TestFizzBuzzLevel2(t *testing.T){
+	input := 2
+	got := FizzbuzzLevel1(input)
+	want := "2"
+	if got != want {
+		t.Errorf("got %q but want %q", got, want)
+	}
+}
