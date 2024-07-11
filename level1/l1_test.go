8a59eab6530b391ff064b73afc317b2f56bb1242
Thanakrit Rojsakthavorn <thanakrit.ro@arise.tech>
2024-07-11 09:48:12 +0700

    [level1:R4:12] test 12

--- level1/l1_test.go
+++ level1/l1_test.go
@@ -90,14 +90,14 @@ func TestFizzBuzzLevel11(t *testing.T){
 		t.Errorf("got %q but want %q", got, want)
 	}
 }
-// func TestFizzBuzzLevel12(t *testing.T){
-// 	input := 12
-// 	got := FizzbuzzLevel1(input)
-// 	want := "Fizz"
-// 	if got != want {
-// 		t.Errorf("got %q but want %q", got, want)
-// 	}
-// }
+func TestFizzBuzzLevel12(t *testing.T){
+	input := 12
+	got := FizzbuzzLevel1(input)
+	want := "Fizz"
+	if got != want {
+		t.Errorf("got %q but want %q", got, want)
+	}
+}
 // func TestFizzBuzzLevel13(t *testing.T){
 // 	input := 13
 // 	got := FizzbuzzLevel1(input)
