83909d63c10cd3782dbf1010b00e8746c06c8701
Thanakrit Rojsakthavorn <thanakrit.ro@arise.tech>
2024-07-11 09:37:51 +0700

    [level1:R4:1] test 1

--- level1/l1.go
+++ level1/l1.go
@@ -1,24 +1,6 @@
 package level1
 
-import "strconv"
-
 
 func FizzbuzzLevel1(number int) string {
-	text := ""
-	flag := false
-
-	if number % 3 == 0 {
-		text += "Fizz"
-		flag = true
-	} 
-	
-	if number % 5 == 0 {
-		text += "Buzz"
-		flag = true
-	}
-	
-	if !flag {
-		text = strconv.Itoa(number)
-	}
-	return text
+	return "1"
 }
--- level1/l1_test.go
+++ level1/l1_test.go
@@ -10,115 +10,115 @@ func TestFizzBuzzLevel1(t *testing.T){
 		t.Errorf("got %q but want %q", got, want)
 	}
 }
-func TestFizzBuzzLevel2(t *testing.T){
-	input := 2
-	got := FizzbuzzLevel1(input)
-	want := "2"
-	if got != want {
-		t.Errorf("got %q but want %q", got, want)
-	}
-}
-func TestFizzBuzzLevel3(t *testing.T){
-	input := 3
-	got := FizzbuzzLevel1(input)
-	want := "Fizz"
-	if got != want {
-		t.Errorf("got %q but want %q", got, want)
-	}
-}
-func TestFizzBuzzLevel4(t *testing.T){
-	input := 4
-	got := FizzbuzzLevel1(input)
-	want := "4"
-	if got != want {
-		t.Errorf("got %q but want %q", got, want)
-	}
-}
-func TestFizzBuzzLevel5(t *testing.T){
-	input := 5
-	got := FizzbuzzLevel1(input)
-	want := "Buzz"
-	if got != want {
-		t.Errorf("got %q but want %q", got, want)
-	}
-}
-func TestFizzBuzzLevel6(t *testing.T){
-	input := 6
-	got := FizzbuzzLevel1(input)
-	want := "Fizz"
-	if got != want {
-		t.Errorf("got %q but want %q", got, want)
-	}
-}
-func TestFizzBuzzLevel7(t *testing.T){
-	input := 7
-	got := FizzbuzzLevel1(input)
-	want := "7"
-	if got != want {
-		t.Errorf("got %q but want %q", got, want)
-	}
-}
-func TestFizzBuzzLevel8(t *testing.T){
-	input := 8
-	got := FizzbuzzLevel1(input)
-	want := "8"
-	if got != want {
-		t.Errorf("got %q but want %q", got, want)
-	}
-}
-func TestFizzBuzzLevel9(t *testing.T){
-	input := 9
-	got := FizzbuzzLevel1(input)
-	want := "Fizz"
-	if got != want {
-		t.Errorf("got %q but want %q", got, want)
-	}
-}
-func TestFizzBuzzLevel10(t *testing.T){
-	input := 10
-	got := FizzbuzzLevel1(input)
-	want := "Buzz"
-	if got != want {
-		t.Errorf("got %q but want %q", got, want)
-	}
-}
-func TestFizzBuzzLevel11(t *testing.T){
-	input := 11
-	got := FizzbuzzLevel1(input)
-	want := "11"
-	if got != want {
-		t.Errorf("got %q but want %q", got, want)
-	}
-}
-func TestFizzBuzzLevel12(t *testing.T){
-	input := 12
-	got := FizzbuzzLevel1(input)
-	want := "Fizz"
-	if got != want {
-		t.Errorf("got %q but want %q", got, want)
-	}
-}
-func TestFizzBuzzLevel13(t *testing.T){
-	input := 13
-	got := FizzbuzzLevel1(input)
-	want := "13"
-	if got != want {
-		t.Errorf("got %q but want %q", got, want)
-	}
-}
-func TestFizzBuzzLevel14(t *testing.T){
-	input := 14
-	got := FizzbuzzLevel1(input)
-	want := "14"
-	if got != want {
-		t.Errorf("got %q but want %q", got, want)
-	}
-}
-func TestFizzBuzzLevel15(t *testing.T){
-	input := 15
-	got := FizzbuzzLevel1(input)
-	want := "FizzBuzz"
-	if got != want {
-		t.Errorf("got %q but want %q", got, want)
-	}
-}
+// func TestFizzBuzzLevel2(t *testing.T){
+// 	input := 2
+// 	got := FizzbuzzLevel1(input)
+// 	want := "2"
+// 	if got != want {
+// 		t.Errorf("got %q but want %q", got, want)
+// 	}
+// }
+// func TestFizzBuzzLevel3(t *testing.T){
+// 	input := 3
+// 	got := FizzbuzzLevel1(input)
+// 	want := "Fizz"
+// 	if got != want {
+// 		t.Errorf("got %q but want %q", got, want)
+// 	}
+// }
+// func TestFizzBuzzLevel4(t *testing.T){
+// 	input := 4
+// 	got := FizzbuzzLevel1(input)
+// 	want := "4"
+// 	if got != want {
+// 		t.Errorf("got %q but want %q", got, want)
+// 	}
+// }
+// func TestFizzBuzzLevel5(t *testing.T){
+// 	input := 5
+// 	got := FizzbuzzLevel1(input)
+// 	want := "Buzz"
+// 	if got != want {
+// 		t.Errorf("got %q but want %q", got, want)
+// 	}
+// }
+// func TestFizzBuzzLevel6(t *testing.T){
+// 	input := 6
+// 	got := FizzbuzzLevel1(input)
+// 	want := "Fizz"
+// 	if got != want {
+// 		t.Errorf("got %q but want %q", got, want)
+// 	}
+// }
+// func TestFizzBuzzLevel7(t *testing.T){
+// 	input := 7
+// 	got := FizzbuzzLevel1(input)
+// 	want := "7"
+// 	if got != want {
+// 		t.Errorf("got %q but want %q", got, want)
+// 	}
+// }
+// func TestFizzBuzzLevel8(t *testing.T){
+// 	input := 8
+// 	got := FizzbuzzLevel1(input)
+// 	want := "8"
+// 	if got != want {
+// 		t.Errorf("got %q but want %q", got, want)
+// 	}
+// }
+// func TestFizzBuzzLevel9(t *testing.T){
+// 	input := 9
+// 	got := FizzbuzzLevel1(input)
+// 	want := "Fizz"
+// 	if got != want {
+// 		t.Errorf("got %q but want %q", got, want)
+// 	}
+// }
+// func TestFizzBuzzLevel10(t *testing.T){
+// 	input := 10
+// 	got := FizzbuzzLevel1(input)
+// 	want := "Buzz"
+// 	if got != want {
+// 		t.Errorf("got %q but want %q", got, want)
+// 	}
+// }
+// func TestFizzBuzzLevel11(t *testing.T){
+// 	input := 11
+// 	got := FizzbuzzLevel1(input)
+// 	want := "11"
+// 	if got != want {
+// 		t.Errorf("got %q but want %q", got, want)
+// 	}
+// }
+// func TestFizzBuzzLevel12(t *testing.T){
+// 	input := 12
+// 	got := FizzbuzzLevel1(input)
+// 	want := "Fizz"
+// 	if got != want {
+// 		t.Errorf("got %q but want %q", got, want)
+// 	}
+// }
+// func TestFizzBuzzLevel13(t *testing.T){
+// 	input := 13
+// 	got := FizzbuzzLevel1(input)
+// 	want := "13"
+// 	if got != want {
+// 		t.Errorf("got %q but want %q", got, want)
+// 	}
+// }
+// func TestFizzBuzzLevel14(t *testing.T){
+// 	input := 14
+// 	got := FizzbuzzLevel1(input)
+// 	want := "14"
+// 	if got != want {
+// 		t.Errorf("got %q but want %q", got, want)
+// 	}
+// }
+// func TestFizzBuzzLevel15(t *testing.T){
+// 	input := 15
+// 	got := FizzbuzzLevel1(input)
+// 	want := "FizzBuzz"
+// 	if got != want {
+// 		t.Errorf("got %q but want %q", got, want)
+// 	}
+// }
